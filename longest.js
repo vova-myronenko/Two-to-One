1078e0444d10c00a998dd0a1f8fd70cc8e1c8460
Volodymyr Myronenko <78821570+vova-myronenko@users.noreply.github.com>
2022-09-25 17:17:30 +0300

    Update longest.js

--- longest.js
+++ longest.js
@@ -4,8 +4,9 @@ function longest(s1, s2) {
   let sorted = arr.sort();
   let result = [];
     for (let i = 0; i < sorted.length; i++){
-      if (sorted[i] !== sorted[i+1])
-        result.push(sorted[i]); 
+      if (sorted[i] !== sorted[i+1]) {
+        result.push(sorted[i]);
+      }
     }
-       return result.join('');
+  return result.join('');
 }
